d27fcc36bcc1445b9c80dd253f23c03d021d3114
sy1ntexx <sy1ntexx@gmail.com>
2021-06-25 15:03:54 +0300

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
-use winapi::um::winnt::{
-    DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH,
-    DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
-};
 use winapi::um::consoleapi::AllocConsole;
+use winapi::um::winnt::DLL_PROCESS_ATTACH;
 use winapi::shared::minwindef::{BOOL, DWORD, FALSE, HINSTANCE, LPVOID, TRUE};
 
 #[allow(unused_variables)]
 #[no_mangle]
-unsafe extern "C" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lp_reserved: LPVOID) -> BOOL {
+unsafe extern "C" fn DllMain(
+    hinst_dll: HINSTANCE,
+    fdw_reason: DWORD,
+    lp_reserved: LPVOID) -> BOOL
+{
     match fdw_reason {
+        // Spawns new thread when attaching to the process instead of blocking DllMain
         DLL_PROCESS_ATTACH => {
             std::thread::spawn(|| unsafe {
                 main_thread();
             });
         }
-        DLL_PROCESS_DETACH => {}
-        DLL_THREAD_ATTACH => {}
-        DLL_THREAD_DETACH => {}
+        // DLL_PROCESS_DETACH => {}
+        // DLL_THREAD_ATTACH => {}
+        // DLL_THREAD_DETACH => {}
         _ => { return FALSE; }
     }
     TRUE
@@ -25,6 +27,8 @@ unsafe extern "C" fn DllMain(hinst_dll: HINSTANCE, fdw_reason: DWORD, lp_reserve
 unsafe fn main_thread() {
     AllocConsole();
     println!("Hello from #RustLang!");
+
+    // Put your code here
 
     loop { }
 }
